9774383f691a6cbfcb594d0a524fee8b94a78802
Leeky0615 <rbdus7174@naver.com>
2022-03-08 17:21:19 +0900

    add: 0308_규연_1049

--- src/solutions/그리디알고리즘/기타줄_1049/규연/Sol_GuitarString.java
+++ src/solutions/그리디알고리즘/기타줄_1049/규연/Sol_GuitarString.java
@@ -3,12 +3,43 @@ package solutions.그리디알고리즘.기타줄_1049.규연;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Sol_GuitarString {
+    static int N, M;
+    static int minPack = 1001;
+    static int minPiece = 1001;
 
-
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        initialize(br);
+        System.out.println(solve());
     }
+
+    private static void initialize(BufferedReader br) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int pack = Integer.parseInt(st.nextToken());
+            int piece = Integer.parseInt(st.nextToken());
+            if (minPack > pack) {
+                minPack = pack;
+            }
+            if (minPiece > piece) {
+                minPiece = piece;
+            }
+        }
+    }
+
+    private static int solve() {
+        return Collections.min(Arrays.asList(
+                N * minPiece,
+                ((N / 6) + 1) * minPack,
+                (N / 6) * minPack + (N % 6) * minPiece));
+    }
+
 }

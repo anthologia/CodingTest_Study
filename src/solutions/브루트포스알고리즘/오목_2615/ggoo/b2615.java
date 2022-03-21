eed56bc026169afd8e5413b69676751beaf31ed2
greeng00se <thegreengoose7@gmail.com>
2022-03-21 13:23:18 +0900

    add: 0321_ggoo

--- src/solutions/브루트포스알고리즘/오목_2615/ggoo/b2615.java
+++ src/solutions/브루트포스알고리즘/오목_2615/ggoo/b2615.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class b2615 {
 
     static int[][] data;
     static int[] dr = {-1, 0, 1, 1};

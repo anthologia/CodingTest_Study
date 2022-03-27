5647fc4ea657be27b5f2255b829c008b74d8f2c5
Leeky0615 <rbdus7174@naver.com>
2022-03-27 15:55:19 +0900

    add: 0327_규연

--- src/solutions/이분탐색/드래곤앤던전_16434/규연/Sol_DragonAndDungeon.java
+++ src/solutions/이분탐색/드래곤앤던전_16434/규연/Sol_DragonAndDungeon.java
@@ -4,32 +4,70 @@ import java.io.*;
 import java.util.*;
 
 public class Sol_DragonAndDungeon {
+    static class Room {
+        int t;
+        int a;
+        int h;
+
+        public Room(int[] info) {
+            this.t = info[0];
+            this.a = info[1];
+            this.h = info[2];
+        }
+    }
+
     static class Source {
         int n;
-        int hatk;
-        int[] dungeon;
+        long hatk;
+        Room[] dungeon;
+
         public Source(BufferedReader br) throws IOException {
             StringTokenizer st = new StringTokenizer(br.readLine());
             n = Integer.parseInt(st.nextToken());
-            hatk = Integer.parseInt(st.nextToken());
-            dungeon = Arrays.stream(br.readLine().split(" "))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-
+            hatk = Long.parseLong(st.nextToken());
+            dungeon = new Room[n];
+            for (int i = 0; i < n; i++) {
+                dungeon[i] = new Room(
+                        Arrays.stream(br.readLine().split(" "))
+                                .mapToInt(Integer::parseInt)
+                                .toArray());
+            }
         }
     }
 
     static class Solution {
-        public void solution(int n, int hatk, int[] dungeon) {
-            int HmaxHp = 0;
+        public long solution(int n, long hatk, Room[] dungeon) {
+            long left = 0;
+            long right = Long.MAX_VALUE;
+            while ((right - left) > 1) {
+                long mid = (left + right) / 2;
+                if (isAvailableHP(dungeon, hatk, mid)) right = mid;
+                else left = mid;
+            }
+            return right;
+        }
 
-
+        public boolean isAvailableHP(Room[] dungeon, long hatk, long maxHp) {
+            long currHp = maxHp;
+            for (Room room : dungeon) {
+                if (room.t == 1) {
+                    int attackTime = (int) Math.ceil((double) room.h / hatk) - 1;
+                    currHp -= room.a * attackTime;
+                    if (currHp >= 0) return false;
+                } else {
+                    hatk += room.a;
+                    currHp += room.h;
+                    if (currHp > maxHp) currHp = maxHp;
+                }
+            }
+            return true;
         }
     }
 
     public static void main(String[] args) throws IOException {
         Source s = new Source(new BufferedReader(new InputStreamReader(System.in)));
-        new Solution().solution(s.n, s.hatk, s.dungeon);
+        long ans = new Solution().solution(s.n, s.hatk, s.dungeon);
+        System.out.println(ans);
 
     }
 }
